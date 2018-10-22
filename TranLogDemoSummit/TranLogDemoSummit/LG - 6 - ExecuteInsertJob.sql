74d4abf9436e64e00107fc6a6351dfad5a027fe3
skreebydba <skreebydba@users.noreply.github.com>
2018-10-22 21:57:26 +0000

    Deleted the Rest of the Old Files

--- TranLogDemoSummit/TranLogDemoSummit/LG - 6 - ExecuteInsertJob.sql
+++
@@ -1,3 +0,0 @@
-USE msdb;
-
-EXECUTE sp_start_job @job_name = 'InsertRowsToTestTables';
--- TranLogDemoSummit/TranLogDemoSummit/LG - 8 - BackupLogs.sql
+++
@@ -1,4 +0,0 @@
-USE master;
-
-BACKUP LOG BigLog TO DISK = 'D:\Backup\BigLog.trn' WITH COMPRESSION, STATS = 5;
-BACKUP LOG SmallLog TO DISK = 'D:\Backup\SmallLog.trn' WITH COMPRESSION, STATS = 5;
