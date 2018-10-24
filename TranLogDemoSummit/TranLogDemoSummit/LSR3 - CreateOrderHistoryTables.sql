d200cec5e51c5447bd9bcb1f4a1437fb885d5084
skreebydba <skreebydba@users.noreply.github.com>
2018-10-24 04:00:20 +0000

    Updated delete scripts

--- TranLogDemoSummit/TranLogDemoSummit/LSR3 - CreateOrderHistoryTables.sql
+++ TranLogDemoSummit/TranLogDemoSummit/LSR3 - CreateOrderHistoryTables.sql
@@ -17,3 +17,20 @@ CREATE TABLE OrderHistorySingle
 ,CustomerId INT
 ,CONSTRAINT PK_OrderHistorySingle_OrderId PRIMARY KEY CLUSTERED(OrderId));
 
+--Current LSN	nvarchar	no	46
+--Transaction ID	nvarchar	no	28
+--Xact ID	bigint	no	8
+--Transaction Name	nvarchar	no	66
+--Operation	nvarchar	no	62
+--Context	nvarchar	no	62
+--Description	nvarchar	no	512
+--Previous LSN	nvarchar	no	46
+--AllocUnitName	nvarchar	no	774
+--Page ID	nvarchar	no	28
+--Slot ID	int	no	4
+--Begin Time	nvarchar	no	48
+--Database Name	nvarchar	no	258
+--Number of Locks	smallint	no	2
+--Lock Information	nvarchar	no	512
+--New Split Page	nvarchar	no	28
+
---
+++ TranLogDemoSummit/TranLogDemoSummit/LSR7 - DeleteOrderHistoryBatchedTableByDate.sql
@@ -0,0 +1,104 @@
+USE TranLogDemo_Batched;
+
+--DELETE FROM tempdb.dbo.TranLogBatchedLog;
+
+DECLARE @mindate DATETIME,
+@maxdate DATETIME,
+@maxlsn NVARCHAR(46);
+
+SELECT @maxlsn = CONCAT(N'0x',MAX([Current LSN])) FROM fn_dblog(NULL,NULL);
+
+SELECT @mindate = MIN(OrderDate)
+FROM OrderHistoryBatched;
+
+SELECT @maxdate = DATEADD(MONTH,-2,CURRENT_TIMESTAMP);
+
+WHILE @mindate < @maxdate
+BEGIN
+
+	BEGIN TRANSACTION
+		
+		DELETE FROM OrderHistoryBatched
+		WHERE OrderDate <= @mindate;
+
+		SELECT @mindate = DATEADD(MONTH,3,@mindate);
+
+	COMMIT TRANSACTION
+
+END
+
+--INSERT INTO tempdb.dbo.TranLogBatchedLog
+--(CurrentLSN
+--,TransactionID
+--,XactID
+--,TransactionName
+--,Operation
+--,Context
+--,[Description]
+--,PreviousLSN
+--,AllocUnitName
+--,PageID
+--,SlotID
+--,BeginTime
+--,DatabaseName
+--,NumberOfLocks
+--,LockInformation
+--,NewSplitPage)
+--SELECT [Current LSN]
+--,[Transaction ID]
+--,[Xact ID]
+--,[Transaction Name]
+--,Operation
+--,Context
+--,[Description]
+--,[Previous LSN]
+--,AllocUnitName
+--,[Page ID]
+--,[Slot ID]
+--,[Begin Time]
+--,[Database Name]
+--,[Number of Locks]
+--,[Lock Information]
+--,[New Split Page]
+--INTO #tranlog
+--FROM fn_dblog(@maxlsn,NULL);
+
+--SELECT TransactionID, Operation, Context, COUNT(*) 
+--FROM tempdb.dbo.TranLogBatchedLog
+--WHERE Operation = 'LOP_DELETE_ROWS'
+--AND Context <> 'LCX_INDEX_INTERIOR'
+--GROUP BY TransactionID, Operation, Context
+--ORDER BY TransactionID, COUNT(*) DESC;
+
+--SELECT * FROM 
+--tempdb.dbo.TranLogBatchedLog
+--WHERE TransactionID IN (
+--	SELECT DISTINCT TransactionID 
+--	FROM tempdb.dbo.TranLogBatchedLog
+--	WHERE Operation = 'LOP_DELETE_ROWS')
+--AND Operation = 'LOP_BEGIN_XACT'
+--AND TransactionName = 'user_transaction';
+
+--SELECT TransactionName, COUNT(*) 
+--FROM tempdb.dbo.TranLogBatchedLog
+--GROUP BY TransactionName
+--ORDER BY COUNT(*);
+----WHERE TransactionID IS NOT NULL;
+
+--SELECT * FROM tempdb.dbo.TranLogBatchedLog
+--WHERE TransactionName = 'user_transaction';
+
+--SELECT * FROM tempdb.dbo.TranLogBatchedLog
+--WHERE TransactionID = '0000:001ea01f'
+----AND Context = 'LCX_MARK_AS_GHOST'
+--ORDER BY CurrentLSN;
+
+--SELECT CONVERT(INT, CONVERT(VARBINARY,SUBSTRING(TransactionID, 6, 13), 2)), CHARINDEX(':',TransactionID), LEN(TransactionID) FROM tempdb.dbo.TranLogBatchedLog
+--WHERE TransactionID = N'0000:001ea7dc';
+
+--SELECT CONVERT(INT, CONVERT(VARBINARY, 'F535BA', 2))
+----F535BA
+
+--DBCC PAGE(11,1,6266,3) WITH TABLERESULTS;
+
+--SELECT DB_ID(N'TranLogDemo_Batched')
